6c8c831d06cb792be7d9173d97280ea1d209fffc
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2024-04-17 04:16:12 +0300

    29/35
    tle pe 5 teste si 1 wa
    k = 1 merge tot

--- oferta.cpp
+++ oferta.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -14,31 +15,77 @@ ofstream fout("oferta.out");
 int main() {
     fin >> n >> k;
     vector<double> c(n + 1, 0);
-    vector<double> dp(n + 1, 0);
+    set<double> dp1;
+    set<double> dp2;
+    set<double> dp3;
+    set<double> dp_nou;
+
     for (int i = 1; i <= n; ++i) {
         fin >> c[i];
     }
-    // SA TESTEZI DOAR k = 1
-    if (k != 1) {
-        fout << -1;
-        return 0;
-    }
-    dp[1] = c[1];
-    dp[2] = c[1] + c[2] - min(c[1], c[2]) / 2;
+    // dpi e cu i nivele in spate in piramida/casuta variantelor
+    dp3.insert(0);
+    dp2.insert(c[1]);
+    dp1.insert(c[1] + c[2] - min(c[1], c[2]) / 2);
+    dp1.insert(c[1] + c[2]);
 
-    double unu, doi, trei;
+    double unu, doi, trei, maxi_local;
+
     for (int i = 3; i <= n; ++i) {
-        unu = dp[i - 1] + c[i];
-        doi = dp[i - 2] + c[i - 1] + c[i] - min(c[i - 1], c[i]) / 2;
-        trei = dp[i - 3] + c[i] + c[i - 1] + c[i - 2] -
-               min(c[i], min(c[i - 1], c[i - 2]));
-        dp[i] = min(unu, min(doi, trei));
+        unu = c[i];
+        doi = c[i - 1] + c[i] - min(c[i - 1], c[i]) / 2;
+        trei = c[i] + c[i - 1] + c[i - 2] - min(c[i], min(c[i - 1], c[i - 2]));
+        for (double x : dp3) {
+            if (dp_nou.size() < k) {
+                dp_nou.insert(x + trei);  // adaugam pentru nivelul 3
+            } else {
+                maxi_local = *(dp_nou.rbegin());
+                if (x + trei < maxi_local) {
+                    dp_nou.erase(maxi_local);
+                    dp_nou.insert(x + trei);
+                }
+            }
+        }
+
+        for (double x : dp2) {
+            if (dp_nou.size() < k) {
+                dp_nou.insert(x + doi);  // adaugam pentru nivelul 2
+            } else {
+                maxi_local = *(dp_nou.rbegin());
+                if (x + doi < maxi_local) {
+                    dp_nou.erase(maxi_local);
+                    dp_nou.insert(x + doi);
+                }
+            }
+        }
+
+        for (double x : dp1) {
+            if (dp_nou.size() < k) {
+                dp_nou.insert(x + unu);  // adaugam pentru nivelul 1
+            } else {
+                maxi_local = *(dp_nou.rbegin());
+                if (x + unu < maxi_local) {
+                    dp_nou.erase(maxi_local);
+                    dp_nou.insert(x + unu);
+                }
+            }
+        }
+
+        dp3 = dp2;
+        dp2 = dp1;
+        dp1 = dp_nou;
+        dp_nou.clear();
+    }
+    // if i e 1 sau 2
+    int contor = 0;
+    for (double x : dp1) {
+        ++contor;
+        if (contor == k) fout << fixed << setprecision(1) << x << '\n';
     }
 
-    // for (int i = 1; i <= n; ++i) {
-    //     cout << dp[i] << ' ';
-    // }
-    // cout << endl;
-    fout << fixed << setprecision(1) << dp[n] << "\n";
+    if (contor < k) {
+        fout << -1;
+    }
+
     return 0;
 }
